651c2d67c9b016ead61da445f240f3d159eaf587
jastinesanchez <jastine.sanchez61726@gmail.com>
2021-01-20 13:54:09 -0800

    more stuff yo

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -2,12 +2,12 @@
 
     What we need to change:
 
-    Day of week
-    Coffee Name
-    Coffee Pic
-    Alt tag
-    Coffee Description
-    Color for coffee
+    Day of week - day
+    Coffee Name - name
+    Coffee Pic - pic
+    Alt tag - alt
+    Coffee Description -desc
+    Color for coffee - color
 
 
 
@@ -17,17 +17,55 @@
 let myDate = new Date ();
 let myDay = myDate.getDay();
 let today = "";
+let coffee = "";
+
+function coffeeTemplate(coffee){
+    let myReturn = "";
+
+    myReturn = `
+    <p>
+    <img src="${coffee.pic}" alt="Our ${coffee.name}" id="coffee" />
+    <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">${coffee.name}</strong>
+    ${coffee.desc}
+
+    
+    </p>
+    
+    
+    
+    `;
+
+
+    return myReturn;
+
+}
 
 switch(myDay){
 
     case 3:
         today = "Wednesday";
+
+        coffee = {
+            color: "brown",
+            name: "Drip",
+            pic: "images/drip.jpg",
+            alt: "A pick of a drip coffee.",
+            day: "Wednesday",
+            desc: "Some days you just need the drip...",
+
+
+
+        };
+
+
     break;
 
 }
 
 
+console.log(coffee);
 
+document.getElementById("coffee-output").innerHTML = coffeeTemplate(coffee);
 
 
 
